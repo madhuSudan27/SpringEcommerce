b01864c191378cb36c3b2c0b54215d95c3550a9a
madhuSudan27 <122591446+madhuSudan27@users.noreply.github.com>
2025-09-01 23:04:27 +0530

    fature get all product

--- src/main/java/com/ecom/controller/ProductController.java
+++ src/main/java/com/ecom/controller/ProductController.java
@@ -2,15 +2,13 @@ package com.ecom.controller;
 
 import com.ecom.model.Product;
 import com.ecom.payload.ProductDTO;
+import com.ecom.payload.ProductResponse;
 import com.ecom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
@@ -25,6 +23,12 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(productDTO);
     
+    }
+
+    @GetMapping("/public/products")
+    public ResponseEntity<ProductResponse> getAllProducts(){
+        ProductResponse productResponse = productService.getAllProducts();
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
 
--- src/main/java/com/ecom/service/ProductService.java
+++ src/main/java/com/ecom/service/ProductService.java
@@ -2,7 +2,9 @@ package com.ecom.service;
 
 import com.ecom.model.Product;
 import com.ecom.payload.ProductDTO;
+import com.ecom.payload.ProductResponse;
 
 public interface ProductService {
     ProductDTO addProduct(Product product, Long categoryId);
+    ProductResponse getAllProducts();
 }
